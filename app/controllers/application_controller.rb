4f0a9f63d66ccab038108e1dc62655bb81c94f27
Daniel  Drumwright <danieldrumwright@gmail.com>
2020-06-23 01:55:35 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,63 +5,64 @@ require 'open-uri'
 require 'pry'
 require 'rack-flash'
 
+
 class ApplicationController < Sinatra::Base
-  use Rack::Flash
+use Rack::Flash
 
-  configure do
-    enable :sessions
-    set :sessions_secret, "secret"
-    set :public_folder, 'public'
-    set :views, 'app/views'
+configure do
+  enable :sessions
+  set :sessions_secret, "secret"
+  set :public_folder, 'public'
+  set :views, 'app/views'
+end
+
+get '/' do
+  erb :root
+end
+
+
+get '/home' do
+      authorize
+      erb :home
   end
 
-  get '/' do
-    erb :root
+  get '/logout' do
+      session.clear
+      redirect '/'
   end
 
 
-  get '/home' do
-        authorize
-        erb :home
+
+helpers do
+
+    def logged_in?
+        !!Cyclist.find_by(id: session[:user_id])
     end
 
-    get '/logout' do
-        session.clear
-        redirect '/'
+    def current_user
+     user = Cyclist.find_by(id: session[:user_id]) if session[:user_id]
     end
 
+    def authenticate(username, password)
+          user = Cyclist.find_by(username: username)
+          session[:user_id] = user.id
+          user
+     end
 
+     def authorize
+         current_user
+     end
 
-  helpers do
-
-      def logged_in?
-          !!Cyclist.find_by(id: session[:user_id])
-      end
-
-      def current_user
-       user = Cyclist.find_by(id: session[:user_id]) if session[:user_id]
-      end
-
-      def authenticate(username, password)
-            user = Cyclist.find_by(username: username)
-            session[:user_id] = user.id
-            user
+     def own_run?(ride)
+           current_user == ride.user
        end
 
-       def authorize
-           current_user
-       end
-
-       def own_run?(ride)
-             current_user == ride.user
-         end
-
-         def login_error_messages(errors)
-            if errors
-                erb :'Cyclists/_errors', locals: {errors: errors}
-            end
-        end
+       def login_error_messages(errors)
+          if errors
+              erb :'Cyclists/_errors', locals: {errors: errors}
+          end
+      end
 
 
-    end
+  end
 end
--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,32 +1,38 @@
 class RidesController < ApplicationController
 
   get '/rides' do
-      authorize
+    if logged_in?
       @rides = Ride.all
       erb :'rides/index'
+    else
+      erb :'cyclists/login'
+    end
   end
 
     get '/rides/new' do
-        authorize
+      if logged_in?
         erb :'rides/new'
+      else
+        erb 'cyclists/login'
+      end
     end
 
-  post '/rides/new' do
-         if params[:description] == "" || params[:ride_distance] == ""
-            flash[:error] = "All fields must be filled"
-            redirect to '/rides/new'
-          elsif logged_in? && !params.empty?
-              @rides= current_user.create( description: params[:description], ride_distance: params[:ride_distance])
-              if @rides.save
-                 redirect '/home'
-                  else
-                    flash[:error] = "Your project could not be saved. Try again!"
-                    redirect '/rides/new'
-                  end
-                end
-              end
+    post '/rides' do
+     if params.values.any? {|value| value == ""}
+      erb :'rides/new', locals: {message: "Your missing information!"}
+    elsif logged_in? && !params.empty?
+      @ride = Ride.create(ride_distance: params[:ride_distance], description: params[:description])
+      if @ride.save
+        redirect to '/rides'
+      else
+        flash[:error] = "Your project could not be saved. Try again!"
+        redirect '/rides/new'
+      end 
+    end
+  end
 
-    delete '/rides/:id' do
+
+delete '/rides/:id' do
         ride = Ride.find_by(id: params[:id])
         authorize_user(ride)
         u = current_user
