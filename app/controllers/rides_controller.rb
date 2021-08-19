c22c7601e0c43bd241379aa0d6307efff181b93f
Dan Drumwright <danieldrumwright@gmail.com>
2021-08-19 17:44:37 -0400

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -19,26 +19,25 @@ class RidesController < ApplicationController
     end
 
     delete '/rides/:id' do
-        ride = Ride.find_by(id: params[:id])
-        authorize_user(ride)
+        @ride = Ride.find_by(id: params[:id])
+        authorize_user(@ride)
         u = current_user
-        if ride
-            ride.destroy
+        if @ride
+            @ride.destroy
             redirect "/users/#{u.id}"
         end
     end
 
     get '/rides/:id/edit' do
         @ride = Ride.find_by(id: params[:id])
-        authorize_user(@ride )
         erb :'rides/edit'
     end
 
-    patch '/rides/:id' do
+    patch '/rides/:id/edit' do
         u = current_user
         @ride = Ride.find_by(id: params[:id])
-        authorize_user(  @ride )
-          @ride .update(distance: params[:distance], time: params[:time], mood: params[:mood])
-        redirect "/users/#{u.id}"
+         @ride.update(distance: params[:distance], time: params[:time], mood: params[:mood])
+        # redirect "/users/#{u.id}"
+        redirect '/rides'
     end
 end
